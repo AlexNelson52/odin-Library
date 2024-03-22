25d8f588bbadb841e2db1a89cbd91bfa7c965731
Alex <alex44nelson@yahoo.com>
2024-03-22 04:41:03 -0400

    commit

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const submitForm = document.querySelector('.sumbit-book');
 const FormInfo = document.querySelector('.form-container')
 const inputTitle = document.querySelector('.inputTitle');
 const inputAuthor = document.querySelector('.inputAuthor');
+const inputPages = document.querySelector('.inputPages')
 
 addBookBtn.addEventListener('click', () => {
   formPopUp.style.display = 'flex'
+
 });
 
 function closeForm() {
@@ -18,43 +20,102 @@ FormInfo.addEventListener('submit', (event) => {
   event.preventDefault()
   addBook();
   formPopUp.style.display = 'none'
+
 })
 
 const myLibrary = [];
 
-function book(title, author) {
+function book(title, author, pages) {
   this.title = title,
     this.author = author,
-    myLibrary.push(`Author: ${this.title} Title: ${this.author}`);
+    this.pages = pages,
+    myLibrary.push(`Title: ${this.title} Author: ${this.author} Pages: ${this.pages}`),
+    myLibrary.forEach((element) => {
+      if (element == element) {
+
+        formPopUp.style.display = 'none';
+        const newContainer = document.createElement('div');
+        let removeBtn = document.createElement('button');
+        removeBtn.classList.add('close');
+        removeBtn.textContent = 'X';
+        let h1 = document.createElement('h1');
+        h1.classList.add('bookh1');
+        let p1 = document.createElement('p');
+        p1.classList.add('bookp1');
+        let p2 = document.createElement('p');
+        p2.classList.add('bookp2');
+        newContainer.classList.add('bookContainer')
+        bookCardContainer.appendChild(newContainer)
+        h1.textContent = `${this.title}`;
+        p1.textContent = `Author: ${this.author}`;
+        p2.textContent = `Number of pages: ${this.pages}`;
+        newContainer.appendChild(h1);
+        newContainer.appendChild(p1);
+        newContainer.appendChild(p2);
+        newContainer.appendChild(removeBtn);
+        removeBtn.addEventListener('click', () => {
+          bookCardContainer.removeChild(newContainer)
+        })
+      }
+    })
 };
+
+console.log(myLibrary);
 
 function addBook() {
-  let inputTitle = document.querySelector('.inputTitle');
-  let inputAuthor = document.querySelector('.inputAuthor');
-  let newBook = (`Title: ${inputTitle.value} Author: ${inputAuthor.value}`);
-  formPopUp.style.display = 'none';
-  const newContainer = document.createElement('div')
-  let removeBtn = document.createElement('button')
-  removeBtn.classList.add('close')
-  removeBtn.textContent = 'X'
-  let h1 = document.createElement('h1')
-  let p1 = document.createElement('p')
-  let p2 = document.createElement('p')
-  newContainer.classList.add('bookContainer')
-  bookCardContainer.appendChild(newContainer)
-  h1.textContent = `Title: ${inputTitle.value}`;
-  p2.textContent = `Author: ${inputAuthor.value}`
-  newContainer.appendChild(h1)
-  newContainer.appendChild(p1)
-  newContainer.appendChild(p2)
-  newContainer.appendChild(removeBtn)
-  removeBtn.addEventListener('click', () => {
-    bookCardContainer.removeChild(newContainer)
-  })
-  return book(inputTitle.value, inputAuthor.value)
+  return book(inputTitle.value, inputAuthor.value, inputPages.value)
 };
 
-function addBookToLibrary() {
-  let inputBookTitle = document.querySelector('#inputbook').value
-  card1Header.textContent = inputBookTitle
-};
+// myLibrary.forEach((element) => {
+//   formPopUp.style.display = 'none';
+//   const newContainer = document.createElement('div');
+//   let removeBtn = document.createElement('button');
+//   removeBtn.classList.add('close');
+//   removeBtn.textContent = 'X';
+//   let h1 = document.createElement('h1');
+//   h1.classList.add('bookh1');
+//   let p1 = document.createElement('p');
+//   p1.classList.add('bookp1');
+//   let p2 = document.createElement('p');
+//   p2.classList.add('bookp2');
+//   newContainer.classList.add('bookContainer')
+//   bookCardContainer.appendChild(newContainer)
+//   h1.textContent = `${element}`;
+//   p1.textContent = `Author: ${inputAuthor.value}`;
+//   p2.textContent = `Number of pages: ${inputPages.value}`;
+//   newContainer.appendChild(h1);
+//   newContainer.appendChild(p1);
+//   newContainer.appendChild(p2);
+//   newContainer.appendChild(removeBtn);
+//   removeBtn.addEventListener('click', () => {
+//     bookCardContainer.removeChild(newContainer)
+//   })
+// })
+
+
+// let newBook = (`Title: ${inputTitle.value} Author: ${inputAuthor.value}`);
+// formPopUp.style.display = 'none';
+// const newContainer = document.createElement('div');
+// let removeBtn = document.createElement('button');
+// removeBtn.classList.add('close');
+// removeBtn.textContent = 'X';
+// let h1 = document.createElement('h1');
+// h1.classList.add('bookh1');
+// let p1 = document.createElement('p');
+// p1.classList.add('bookp1');
+// let p2 = document.createElement('p');
+// p2.classList.add('bookp2');
+// newContainer.classList.add('bookContainer')
+// bookCardContainer.appendChild(newContainer)
+// h1.textContent = `${inputTitle.value}`;
+// p1.textContent = `Author: ${inputAuthor.value}`;
+// p2.textContent = `Number of pages: ${inputPages.value}`;
+// newContainer.appendChild(h1);
+// newContainer.appendChild(p1);
+// newContainer.appendChild(p2);
+// newContainer.appendChild(removeBtn);
+// removeBtn.addEventListener('click', () => {
+//   bookCardContainer.removeChild(newContainer)
+// })
+// return book(inputTitle.value, inputAuthor.value, inputPages.value)
+// };
